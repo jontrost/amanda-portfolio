6445a8885c3640cc7497352ed0c182435608b7c1
Jon Trost <jontrost426@gmail.com>
2023-09-23 21:01:59 -0500

    Created basic landing skeleton

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -8,4 +8,25 @@ import { Component } from "@angular/core";
     templateUrl: "./landing.component.html",
     styleUrls: ["./landing.component.scss"]
 })
-export default class LandingComponent {}
+export default class LandingComponent {
+    projects: string[] = [
+        "Project 1",
+        "Project 2",
+        "Project 3",
+        "Project 4",
+        "Project 5",
+        "Project 6",
+        "Project 7",
+        "Project 8",
+        "Project 9",
+        "Project 10",
+        "Project 11"
+    ];
+    // Rename this
+    selectedProjectPreview: string = this.projects[0];
+
+    // Add rxjs observable with debounceTimer that cycles through each preview automatically
+    previewProject(projectIndex: number) {
+        this.selectedProjectPreview = this.projects[projectIndex];
+    }
+}
